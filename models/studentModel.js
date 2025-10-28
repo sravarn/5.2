0600d1ab1c6a50b60ff7d19bdb333ab420fbba37
sravarn <88591164+sravarn@users.noreply.github.com>
2025-10-28 12:12:17 +0530

    Create studentModel.js

---
+++ models/studentModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    course: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const Student = mongoose.model('Student', studentSchema);
+
+module.exports = Student;
